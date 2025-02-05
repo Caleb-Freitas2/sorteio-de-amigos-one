5e20e0ca5f20d185b4f8bed01d552b2171ce66a5
Caleb Montes Silva De Freitas <139081806+Caleb-Freitas2@users.noreply.github.com>
2025-02-05 15:57:22 -0300

    Add files via upload

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,59 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Digite um nome válido!");
+        return;
+    }
+
+    if (amigos.includes(nome)) {
+        alert("Esse nome já foi adicionado!");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo, index) => {
+        const item = document.createElement("li");
+        item.textContent = amigo;
+
+        const botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "❌";
+        botaoRemover.classList.add("botao-remover");
+        botaoRemover.onclick = () => removerAmigo(index);
+
+        item.appendChild(botaoRemover);
+        lista.appendChild(item);
+    });
+}
+
+function removerAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos 2 amigos para sortear!");
+        return;
+    }
+
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+
+
+    const listaResultado = document.getElementById("resultado");
+    listaResultado.innerHTML = `<li>🎉 O amigo secreto sorteado foi: <strong>${sorteado}</strong> 🎉</li>`;
+}
+
+
